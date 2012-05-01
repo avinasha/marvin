6123da7277258d0d01d444d550cc6619a600f38c
Avinasha Shastry <me@avinasha.com>
2012-05-01 10:51:20 -0400

    Adding readme and updating app

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ module Marvin
     post '/supportbee' do
       json = JSON.parse(request.body.read.to_s)
       payload = json['payload']
-      if payload['action_type'] == 'ticket_created'
+      if payload['action_type'] == 'ticket.created'
         speech = "[#{payload['company']['name']}:New Ticket] #{payload['ticket']['subject']}\n https://#{payload['company']['subdomain']}.supportbee.com/tickets/#{payload['ticket']['id']}"
         CAMPFIRE.speak speech
       end
